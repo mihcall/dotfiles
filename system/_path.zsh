49566252d11337dba3598d4032cefaa8f59f29f3
mihcall <michal@sliwon.io>
2019-08-01 14:58:09 +0200

    Change paths back to normal

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:/Users/mihcall/Library/Python/2.7/bin:~/.bin:$PATH"
+export PATH="./bin:/usr/local/Cellar:/usr/local/bin:/usr/local/sbin:$ZSH/bin:~/.bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
